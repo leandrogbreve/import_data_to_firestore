b650cc018ef370d297944917c5942a12fb4781f3
Leandro Breve <leandrogbreve@gmail.com>
2022-05-01 20:25:12 -0300

    function main isolated to best practices.
    possibility to call the program with the --timestamps flag

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import getopt
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -12,16 +14,25 @@ db = firestore.client()
 # read file "data.json" which must contain the json with collection and docs to be imported.
 # check readme for example
 
-with open('./data.json') as file:
-    data = json.load(file)
+def main(argv):
+    with open('./data.json') as file:
+        data = json.load(file)
 
-    # first level of file refers to a collection
-    for collection in data:
-        # second level refers to each doc to be inserted in a collection
-        for doc in data[collection]:
-            # If needed, add fields into the doc being imported here,
-            # like log fields (createdAt, uploadedAt), etc.
-            # doc['field'] = value
+        # first level of file refers to a collection
+        for collection in data:
+            # second level refers to each doc to be inserted in a collection
+            for doc in data[collection]:
+                # If needed, add fields into the doc being imported here,
 
-            # inserts the doc in firestore
-            db.collection(collection).document().set(doc)
+                # If --timestamps flag was passed by the user, the fields "createdAt" 
+                # and "uploadedAt"  will be inserted in the doc with the current server timestamp
+                if('--timestamps' in argv):
+                    doc['createdAt'] = firestore.SERVER_TIMESTAMP
+                    doc['uploadedAt'] = firestore.SERVER_TIMESTAMP
+
+                # adds the doc to the firestore database
+                db.collection(collection).document().set(doc)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
